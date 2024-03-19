3650ecc9f98dbf0e6b5de77aecdf82008bcbf8ec
SvyatoslavGavrilov <svyatogavr@gmail.com>
2024-03-19 10:30:48 +0300

    Remake_Of_Renderer

--- sfml.cpp
+++ sfml.cpp
@@ -20,6 +20,7 @@ struct IniSettings{
 	std::string edge_side;
 	std::string edge_td;
 	short font_size;
+	std::string font;
 };
 
 IniSettings IniDownloader(std::string filename){
@@ -31,37 +32,69 @@ IniSettings IniDownloader(std::string filename){
 	>> settings.edge_side
 	>> settings.edge_td
 	>> settings.font_size
+	>> settings.font
 	;
 	stngs_strm.close();
 	return settings;
 }
 
+class Board{
 
-std::string Board_Drawer(BoardProp boad, bool count=0){
-	std::string map;
-	map += boad.edge_side;
-	for(int i=0; i<boad.dim_x; i++)map += boad.edge_td;
-	map += boad.edge_side + "\n";
-	for(int y=0; y<boad.dim_y; y++){
-		map += boad.edge_side;
-		map += [](const char str, int times)
-					{std::stringstream stream; for (int i = 0; i < times; i++) stream << str; return stream.str();}
-		('.', boad.dim_x);
+	BoardProp stngs;
+	std::vector<std::string> brd;
 
-		std::string line_num;
-		line_num += [](bool c, int n){
-			std::stringstream conv;
-			if(c)conv<<n+1;
-			return conv.str();}(count, y);
+	public:
 
-		map += boad.edge_side + line_num + "\n";
+	Board (BoardProp boad){
+		stngs = boad;
+		for(int i = 0; i<stngs.dim_y; i++){
+			std::string str="";
+			for(int j = 0; j<stngs.dim_x; j++)str+=" ";
+			brd.push_back(str);
+		}
 	}
 
-	map += boad.edge_side;
-		for(int i=0; i<boad.dim_x; i++)map += boad.edge_td;
-		map += boad.edge_side + '\n';
+	std::string Board_Drawer(bool count=0){
+		std::string map;
+		map += stngs.edge_side;
+		for(int i=0; i<stngs.dim_x; i++)map += stngs.edge_td;
+		map += stngs.edge_side + "\n";
+		for(int y=0; y<stngs.dim_y; y++){
+			map += stngs.edge_side;
 
-	return map;
+			/*map += [](const char str, int times)
+						{std::stringstream stream; for (int i = 0; i < times; i++) stream << str; return stream.str();}
+			('.', stngs.dim_x);
+			*/
+
+			map += brd[y];
+
+			std::string line_num;
+			line_num += [](bool c, int n){
+				std::stringstream conv;
+				if(c)conv<<n+1;
+				return conv.str();}(count, y);
+
+			map += stngs.edge_side + line_num + "\n";
+		}
+
+		map += stngs.edge_side;
+			for(int i=0; i<stngs.dim_x; i++)map += stngs.edge_td;
+			map += stngs.edge_side + '\n';
+
+		return map;
+	};
+
+	void set(char sym, int x, int y){
+
+		brd[y][x] = sym;
+
+	}
+
+	void reset_stngs(BoardProp stngs){
+		this->stngs = stngs;
+	}
+
 };
 
 
@@ -71,32 +104,37 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(1080,760), "Hello World");
 
-    BoardProp board;
+    BoardProp brd_stngs;
 
     // Load settings
     IniSettings settings = IniDownloader("stngs.ini");
 
-    board.dim_x = settings.dim_x;
-    board.dim_y = settings.dim_y;
-    board.edge_side = settings.edge_side;
-    board.edge_td = settings.edge_td;
+    brd_stngs.dim_x = settings.dim_x;
+    brd_stngs.dim_y = settings.dim_y;
+    brd_stngs.edge_side = settings.edge_side;
+    brd_stngs.edge_td = settings.edge_td;
 
 
     // Load font from fonts/
     sf::Font font;
-    font.loadFromFile("fonts/1942.ttf");
+    font.loadFromFile(settings.font);
     sf::Text text;
     text.setFont(font);
 
+    Board brd(brd_stngs); //create Board class (class containing what is displayed);
 
-    //setting text
+    brd.set('A', 1, 1);
+    brd.set('B', 2, 1);
+    brd.set('C', 4, 4);
 
-    text.setString(Board_Drawer(board, 1));
+    //get things drawn
+    text.setString(brd.Board_Drawer());
     text.setCharacterSize(settings.font_size);
 
     text.setFillColor(sf::Color::Red);
 
-    //window.display();
+
+
 
     while (window.isOpen())
     {
